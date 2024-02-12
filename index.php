9c06555a4eb1b1a10e0c2280425daa81db51bf04
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-02-12 02:17:57 +0100

    Form

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        Password Generator
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <form action="" method="GET">
+                               
+                                <div class="mb-3">
+                                    <label for="length" class="visually-hidden">Lunghezza password</label>
+                                    <input 
+                                    id="length"
+                                    name="length"
+                                    type="number" 
+                                    class="form-control"  placeholder="Inserisci lunghezza">
+                                </div>
+                                <div >
+                                    <button type="submit" class="btn btn-primary mb-3">Genera Password</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+        </div>
+    </main>
+
+    <footer>
+         
+    </footer>
+</body>
+</html>
